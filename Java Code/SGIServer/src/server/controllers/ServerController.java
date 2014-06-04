4962b6edc5e9513298473094ab6a645e3544021d
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 22:57:08 +0300

    Update ServerController.java

--- Java Code/SGIServer/src/server/controllers/ServerController.java
+++ Java Code/SGIServer/src/server/controllers/ServerController.java
@@ -1,64 +1,61 @@
 package server.controllers;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Date;
 import ocsf.server.AbstractServer;
 import ocsf.server.ConnectionToClient;
+import server.controllers.RequestController.REQUESTS;
+import server.database.Database;
 import server.gui.ServerGUI;
+import server.models.DataPackage;
+import sgi.entities.*;
+
 import java.awt.event.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
 public class ServerController extends AbstractServer {
 
 	private ServerGUI _gui;
 	private ConfigurationController _config;
+	private RequestController _request;
+	private Database _db;
+	private int _connectedClients = 0;
 	
 	public void ShowGUI() {
 		_gui.setVisible(true);
 	}
+	
+	public int getNumberOfConnectionsToClients() { return _connectedClients; }
 	
 	public ServerController(int port, ConfigurationController cc) {
 		super(port);
 		startLog();
 		appendToLog("Server initialize with port " + port);
 		_config = cc;
-		_gui = new ServerGUI(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (e.getActionCommand().equals("Start server")) {
-					try {
-						listen();
-						_gui.setServerStop();
-					} catch (IOException e1) {}
-				}
-				if (e.getActionCommand().equals("Stop server")) {
-					try {
-						if (getNumberOfClients() > 0) {
-							int ans = JOptionPane.showConfirmDialog(null, 
-									"There are connected clients.\n" +
-									"Stopping the server will disconnect all clients." +
-									"Are you sure?", "SGI Server",JOptionPane.YES_NO_OPTION);
-							if (ans==JOptionPane.NO_OPTION) return;
-						}
-						close();
-						_gui.setServerStart();
-					} catch (IOException e1) {}
-				}
-				if (e.getActionCommand().equals("Server configuration")) {
-					_config.showConfigurationWindow();
-				}
-			}});
+		// Initialize DB object
+		_db = new Database(_config.getConfiguration().getHost(),_config.getConfiguration().getDBName(),
+				_config.getConfiguration().getSqlUsername(),_config.getConfiguration().getSqlPassword());
+		_request = new RequestController(_db);
+		_gui = new ServerGUI(new ServerGuiActionListener());
 	}
 
 	protected void clientConnected(ConnectionToClient client) {
 		appendToLog(client.getName() + " connected and got the id: " + client.getId());
+		_connectedClients++;
 	}
 
 	synchronized protected void clientDisconnected(ConnectionToClient client) {
-		appendToLog(client.getName() + " disconnected");		
+		appendToLog(client.getName() + " disconnected");
+		_connectedClients--;
 	}
 
 	protected void serverStarted() {
@@ -74,8 +71,9 @@ public class ServerController extends AbstractServer {
 	}
 
 	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
-		// TODO Auto-generated method stub
-		appendToLog("Message recived from " + client.getName());
+		DataPackage _msg = (DataPackage)msg;
+		appendToLog("Request " + _msg.getRequestType().toString() +  " recived from " + client.getName());
+		_request.handleRequest(_msg, client);
 	}
 	
 	private void appendToLog(String logEntry) {
@@ -102,5 +100,35 @@ public class ServerController extends AbstractServer {
 		    out.close();
 		} catch (IOException e) {}		
 	}
+	
+	// Listeners
+	class ServerGuiActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (e.getActionCommand().equals("Start server")) {
+				try {
+					listen();
+					_gui.setServerStop();
+				} catch (IOException e1) {}
+			}
+			if (e.getActionCommand().equals("Stop server")) {
+				try {
+					if (getNumberOfConnectionsToClients() > 0) {
+						int ans = JOptionPane.showConfirmDialog(null, 
+								"There are connected clients.\n" +
+								"Stopping the server will disconnect all clients." +
+								"Are you sure?", "SGI Server",JOptionPane.YES_NO_OPTION);
+						if (ans==JOptionPane.NO_OPTION) return;
+					}
+					close();
+					_gui.setServerStart();
+				} catch (IOException e1) {}
+			}
+			if (e.getActionCommand().equals("Server configuration")) {
+				_config.showConfigurationWindow();
+			}
+		}	
+	}
 
 }
+
+
