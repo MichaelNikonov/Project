3e7747bd5b4e41000eea2838f2174925976aab89
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:08:24 +0300

    Update PurchaseType.java

--- Java Code/SGIServer/src/sgi/entities/PurchaseType.java
+++ Java Code/SGIServer/src/sgi/entities/PurchaseType.java
@@ -1,16 +1,25 @@
 package sgi.entities;
 
-public class PurchaseType implements IPurchaseType {
+import java.io.Serializable;
 
+public class PurchaseType implements IPurchaseType , Serializable {
+
+	// Variables
+	private static final long serialVersionUID = 891420510746620503L;
 	private int _id;
 	private String _name;
+	// End variables
 	
+	// Constructors
 	public PurchaseType(int id, String name) {
 		_id = id;
 		_name = name;
 	}
+	// End constructors
 	
+	// Methods
 	public int getId() { return _id; }
 	public String getName() { return _name; }
-
+	// End methods
+	
 }
