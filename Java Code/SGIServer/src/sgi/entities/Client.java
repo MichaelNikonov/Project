488d63c6dbb9567e149e552b940d48dcc258feda
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:05:46 +0300

    Update Client.java

--- Java Code/SGIServer/src/sgi/entities/Client.java
+++ Java Code/SGIServer/src/sgi/entities/Client.java
@@ -1,11 +1,17 @@
 package sgi.entities;
 
-public class Client implements IClient {
+import java.io.Serializable;
 
+public class Client implements IClient , Serializable {
+
+	// Variables
+	private static final long serialVersionUID = -2898543220945600108L;
 	private int _id;
 	private String _firstName, _lastName, _phone, _email;
 	private User _user;
+	// End variables
 	
+	// Constructors
 	public Client(int id, String firstName, String lastName, String phone, 
 			String email, User usr) {
 		_id = id;
@@ -15,32 +21,20 @@ public class Client implements IClient {
 		_email = email;
 		_user = usr;
 	}
+	// End constructors
 	
+	// Methods
 	public int getId() { return _id; }
 	public String getFirstName() { return _firstName; }
 	public String getLastName() { return _lastName; }
 	public String getPhone() { return _phone; }
 	public String getEmail() { return _email; }
 	public User getUser() { return _user; }
-
-	public void setFirstName(String value) {
-		_firstName = value;
-	}
-
-	public void setLastName(String value) {
-		_lastName = value;
-	}
-
-	public void setPhone(String value) {
-		_phone = value;
-	}
-
-	public void setEmail(String value) {
-		_email = value;
-	}
-
-	public void setUser(User value) {
-		_user = value;
-	}
+	public void setFirstName(String value) { _firstName = value; }
+	public void setLastName(String value) { _lastName = value; }
+	public void setPhone(String value) { _phone = value; }
+	public void setEmail(String value) { _email = value; }
+	public void setUser(User value) { _user = value; }
+	// End methods
 	
 }
