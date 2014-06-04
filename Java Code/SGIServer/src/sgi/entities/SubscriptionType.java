fed54212a8806cd9893cb5b6092c1af9bb4eae27
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:06:22 +0300

    Update SubscriptionType.java

--- Java Code/SGIServer/src/sgi/entities/SubscriptionType.java
+++ Java Code/SGIServer/src/sgi/entities/SubscriptionType.java
@@ -1,16 +1,25 @@
 package sgi.entities;
 
-public class SubscriptionType {
+import java.io.Serializable;
 
+public class SubscriptionType implements ISubscriptionType, Serializable {
+
+	// Variables
+	private static final long serialVersionUID = -351598073165836561L;
 	private int _id;
 	private String _name;
+	// End variables
 	
+	// Constructors
 	public SubscriptionType(int id, String name) {
 		_id = id;
 		_name = name;
 	}
+	// End constructors
 	
+	// Methods
 	public int getId() { return _id; }
 	public String getName() { return _name; }
+	// End methods
 	
 }
