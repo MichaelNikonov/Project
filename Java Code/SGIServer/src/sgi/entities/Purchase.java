e61b4b73ef62a48d42d6721651055dc537f039ad
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:08:08 +0300

    Update Purchase.java

--- Java Code/SGIServer/src/sgi/entities/Purchase.java
+++ Java Code/SGIServer/src/sgi/entities/Purchase.java
@@ -1,16 +1,21 @@
 package sgi.entities;
 
 import java.util.Date;
+import java.io.Serializable;
 
-public class Purchase implements IPurchase {
+public class Purchase implements IPurchase , Serializable {
 
+	// Variables
+	private static final long serialVersionUID = 2657248592379832598L;
 	private int _id;
 	private SGIImage _image;
 	private Client _client;
 	private PurchaseType _type;
 	private float _price;
 	private Date _datetime;
+	// End variables
 	
+	// Constructors
 	public Purchase(int id, SGIImage img, Client clnt, PurchaseType type, 
 			float price, Date dt) {
 		_id = id;
@@ -20,12 +25,14 @@ public class Purchase implements IPurchase {
 		_price = price;
 		_datetime = dt;
 	}
+	// End constructors
 	
+	// Methods
 	public int getId() { return _id; }
 	public SGIImage getImage() { return _image; }
 	public Client getClient() { return _client; }
 	public PurchaseType getType() { return _type; }
 	public float getPrice() { return _price; }
 	public Date getDateTime() { return _datetime; }
-	
+	// End methods
 }
