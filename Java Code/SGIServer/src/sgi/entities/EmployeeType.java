5d337833ada40ae8c0a0ef649563f962629bfbfa
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:03:37 +0300

    Update EmployeeType.java

--- Java Code/SGIServer/src/sgi/entities/EmployeeType.java
+++ Java Code/SGIServer/src/sgi/entities/EmployeeType.java
@@ -1,16 +1,24 @@
 package sgi.entities;
 
-public class EmployeeType implements IEmployeeType {
+import java.io.Serializable;
 
+public class EmployeeType implements IEmployeeType , Serializable {
+
+	// Variables
+	private static final long serialVersionUID = -39969203460773357L;
 	private int _id;
 	private String _name;
+	// End variables
 	
+	// Constructors
 	public EmployeeType(int id, String name) {
 		_id = id;
 		_name = name;
 	}
+	// End constructors
 	
+	// Methods
 	public int getId() { return _id; }
 	public String getName() { return _name; }
-	
+	// End methods
 }
