fc216a46607defb9dc6b0a742ecb6a6bdb0fc9c5
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:09:36 +0300

    Update Permission.java

--- Java Code/SGIServer/src/sgi/entities/Permission.java
+++ Java Code/SGIServer/src/sgi/entities/Permission.java
@@ -1,16 +1,24 @@
 package sgi.entities;
 
-public class Permission implements IPermission {
+import java.io.Serializable;
 
+public class Permission implements IPermission , Serializable {
+
+	// Variables
+	private static final long serialVersionUID = 359385049596126852L;
 	private int _id;
 	private String _name;
+	// End variables
 	
+	// Constructors
 	public Permission(int id, String name) {
 		_id = id;
 		_name = name;
 	}
+	// End constructors
 	
+	// Methods
 	public int getId() { return _id; }
 	public String getName() { return _name; }
-	
+	// End methods
 }
