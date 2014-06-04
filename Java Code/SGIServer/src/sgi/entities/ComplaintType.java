67035b4cc1b625d72504a2a1bfa1d87c5ac22cd7
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:03:52 +0300

    Update ComplaintType.java

--- Java Code/SGIServer/src/sgi/entities/ComplaintType.java
+++ Java Code/SGIServer/src/sgi/entities/ComplaintType.java
@@ -1,16 +1,24 @@
 package sgi.entities;
 
-public class ComplaintType implements IComplaintType {
+import java.io.Serializable;
 
+public class ComplaintType implements IComplaintType , Serializable {
+	
+	// Variables
+	private static final long serialVersionUID = 3445555093579270277L;
 	private int _id;
 	private String _name;
+	// End variables
 	
+	// Constructors
 	public ComplaintType(int id, String name) {
 		_id = id;
 		_name = name;
 	}
+	// End constructors
 	
+	// Methods
 	public int getId() { return _id; }
 	public String getName() { return _name; }
-
+	// End methods
 }
