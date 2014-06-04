779dea7c9adb123c59d1ab0ca889d2ea1fad276e
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:06:37 +0300

    Update SubscriptionPurchase.java

--- Java Code/SGIServer/src/sgi/entities/SubscriptionPurchase.java
+++ Java Code/SGIServer/src/sgi/entities/SubscriptionPurchase.java
@@ -1,15 +1,20 @@
 package sgi.entities;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class SubscriptionPurchase implements ISubscriptionPurchase {
+public class SubscriptionPurchase implements ISubscriptionPurchase , Serializable {
 	
+	// Variables
+	private static final long serialVersionUID = -7307931061319771239L;
 	private Subscription _subscription;
 	private Client _client;
 	private Date _datetime;
 	private int _usedimgs;
 	private float _price;
+	// End variables
 	
+	// Constructors
 	public SubscriptionPurchase(Subscription sub, Client clnt, Date dt,
 			int usedimgs, float price) {
 		_subscription = sub;
@@ -18,12 +23,14 @@ public class SubscriptionPurchase implements ISubscriptionPurchase {
 		_usedimgs = usedimgs;
 		_price = price;
 	}
+	// End constructors
 	
+	// Methods
 	public Subscription getSubscription() { return _subscription; }
 	public Client getClient() { return _client; }
 	public Date getDateTime() { return _datetime; }
 	public int getUsedImages() { return _usedimgs; }
 	public float getPrice() { return _price; }
-	
 	public void setUsedImages(int value) { _usedimgs = value; }
+	// End methods
 }
