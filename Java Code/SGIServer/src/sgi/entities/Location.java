4c0fc6ce0f8dfdb126a333be0d6bd1a877b76354
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:09:53 +0300

    Update Location.java

--- Java Code/SGIServer/src/sgi/entities/Location.java
+++ Java Code/SGIServer/src/sgi/entities/Location.java
@@ -1,20 +1,28 @@
 package sgi.entities;
 
-public class Location implements ILocation {
+import java.io.Serializable;
+
+public class Location implements ILocation , Serializable {
  
+	// Variables
+	private static final long serialVersionUID = 869142885068811865L;
 	private int _id, _x, _y;
 	private String _name;
+	// End variables
 	
+	// Constructors
 	public Location(int id, int x, int y, String name) {
 		_id = id;
 		_x = x;
 		_y = y;
 		_name = name;
 	}
+	// End constructors
 	
+	// Methods
 	public int getId() { return _id; }
 	public int getX() { return _x; }
 	public int getY() { return _y; }
 	public String getName() { return _name; }
-	
+	// End methods
 }
