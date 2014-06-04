cd12e62f372054f82e1c16b114458bdb50a271f2
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:01:38 +0300

    Update Employee.java

--- Java Code/SGIServer/src/sgi/entities/Employee.java
+++ Java Code/SGIServer/src/sgi/entities/Employee.java
@@ -1,11 +1,17 @@
 package sgi.entities;
 
-public class Employee implements IEmployee {
+import java.io.Serializable;
 
+public class Employee implements IEmployee , Serializable {
+
+	// Variables
+	private static final long serialVersionUID = -7034204849504064940L;
 	private int _id;
 	private String _firstName, _lastName, _phone, _email, _number, _username;
 	private EmployeeType _type;
+	// End variables
 	
+	// Constructors
 	public Employee(int id, String firstName, String lastName, String phone, 
 			String email,String number, EmployeeType type, String username) {
 		_id = id;
@@ -17,7 +23,9 @@ public class Employee implements IEmployee {
 		_username = username;
 		_type = type;
 	}
+	// End constructors
 	
+	// Methods
 	public int getId() { return _id; }
 	public String getFirstName() { return _firstName; }
 	public String getLastName() { return _lastName; }
@@ -26,5 +34,5 @@ public class Employee implements IEmployee {
 	public String getNumber() { return _number; }
 	public EmployeeType getType() { return _type; }
 	public String getUsername() { return _username; }
-
+	// End methods
 }
