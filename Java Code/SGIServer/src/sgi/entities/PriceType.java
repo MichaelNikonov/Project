12b96af11210fc3f04f74d8c58c0ceea4ff07709
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:08:40 +0300

    Update PriceType.java

--- Java Code/SGIServer/src/sgi/entities/PriceType.java
+++ Java Code/SGIServer/src/sgi/entities/PriceType.java
@@ -1,16 +1,24 @@
 package sgi.entities;
 
-public class PriceType implements IPriceType {
+import java.io.Serializable;
 
+public class PriceType implements IPriceType , Serializable {
+
+	// Variables
+	private static final long serialVersionUID = 9149656090422400767L;
 	private int _id;
 	private String _name;
+	// End variables
 	
+	// Constructors
 	public PriceType(int id, String name) {
 		_id = id;
 		_name = name;
 	}
+	// End constructors
 	
+	// Methods
 	public int getId() { return _id; }
 	public String getName() { return _name; }
-	
+	// Methods
 }
