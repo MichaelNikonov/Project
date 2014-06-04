a1c5b9116e3738e9164c8e3cb4e888cc2652e3cd
Ort Braude College Of Engineering - Spring 2014 Lab Project - Group 7 <braude-spring-2014@users.noreply.github.com>
2014-06-04 23:07:31 +0300

    Update Subscription.java

--- Java Code/SGIServer/src/sgi/entities/Subscription.java
+++ Java Code/SGIServer/src/sgi/entities/Subscription.java
@@ -1,25 +1,38 @@
 package sgi.entities;
 
-public class Subscription implements ISubscription {
+import java.io.Serializable;
 
+public class Subscription implements ISubscription , Serializable {
+
+	// Variables
+	private static final long serialVersionUID = 6117965227807088814L;
 	private int _id;
+	private boolean _active;
 	private SubscriptionType _type;
 	private LayerType _layertype;
 	private int _imgs;
+	// End variables
 	
-	public Subscription(int id, SubscriptionType type, LayerType lt, int imgs) {
+	// Constructors
+	public Subscription(int id, SubscriptionType type, LayerType lt, int imgs,boolean active) {
 		_id = id;
 		_type = type;
 		_layertype = lt;
 		_imgs = imgs;
+		_active = active;
 	}
+	// End constructors
 	
+	// Methods
 	public int getId() { return _id; }
 	public SubscriptionType getType() { return _type; }
 	public LayerType getLayerType() { return _layertype; }
 	public int getImageAmount() { return _imgs; }
-	
+	public boolean getActive() { return _active; }
 	public void setImageAmount(int value) { _imgs = value; }
 	public void setLayerType(LayerType value) { _layertype = value; }
 	public void setSubscriptionType(SubscriptionType value) { _type = value; }
+	public void setActive(boolean value) { _active = value; }
+	// End methods
+	
 }
